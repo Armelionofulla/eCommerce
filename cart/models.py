04439338c62582647ca93cbb904d87f0cd89fb3f
Armelio Nofulla <nofullaarmelio01@gmail.com>
2024-04-11 18:50:54 +0200

    implement data into cart page

--- cart/models.py
+++ cart/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+
 from store.models import Product
+
 
 # Create your models here.
 class Cart(models.Model):
@@ -15,6 +17,9 @@ class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
+
+    def sub_total(self):
+        return self.product.price * self.quantity
 
     def __str__(self):
         return self.product
--- cart/views.py
+++ cart/views.py
@@ -1,42 +1,59 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+
 from store.models import Product
 from .models import Cart, CartItem
+
 
 # Create your views here.
 
 def _cart_id(request):
-    cart = request.session.session_key
-    if not cart:
-        cart = request.session.create()
-    return cart
+    cart_id = request.session.get('cart_id')
+    if not cart_id:
+        cart_id = request.session.session_key
+        if not cart_id:
+            request.session.cycle_key()
+            cart_id = request.session.session_key
+        request.session['cart_id'] = cart_id
+    return cart_id
 
 
 def add_cart(request, product_id):
-    product = Product.objects.get(id=product_id)  # get a product
-    try:
-        cart = Cart.objects.get(cart_id=_cart_id(request))  # get the cart using the cart_id present in the session
-    except Cart.DoesNotExist:
-        cart = Cart.objects.create(
-            cart_id=_cart_id(request)
-        )
-    cart.save()
+    product = Product.objects.get(id=product_id)
+    cart_id = _cart_id(request)
 
     try:
-        cart_item = CartItem.objects.get(product=product, cart=cart)
-        cart_item.quantity = 1
-        cart_item.save()
-    except CartItem.DoesNotExist:
-        cart_item = CartItem.objects.create(
-            product=product,
-            quantity= 1,
-            cart = cart
-        )
-        cart_item.save()
-    # return HttpResponse(cart_item.product)
-    # exit()
+        cart = Cart.objects.get(cart_id=cart_id)
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(cart_id=cart_id)
+
+    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product, defaults={'quantity': 0})
+    cart_item.quantity += 1
+    cart_item.save()
+
     return redirect('cart')
 
 
 def cart(request):
-    return render(request, 'store/cart.html')
+    total = 0
+    quantity = 0
+    cart_items = None
+
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+    except Cart.DoesNotExist:
+        pass
+    except ObjectDoesNotExist:
+        pass
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items
+    }
+    return render(request, 'store/cart.html', context)
