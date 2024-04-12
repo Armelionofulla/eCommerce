3e4e7c5fee487ba436691be87fb4eacc411c8acd
Armelio Nofulla <nofullaarmelio01@gmail.com>
2024-04-12 22:23:21 +0200

    account activation with email

--- eCommerceTech/settings.py
+++ eCommerceTech/settings.py
@@ -142,4 +142,14 @@ MESSAGE_TAGS = {
     messages.ERROR: "danger",
 }
 
+# SMTP config
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'nofullaarmelio01@gmail.com'
+EMAIL_HOST_PASSWORD = 'womievypqcolddhs'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+ALLOWED_HOSTS = []
