cda3e37e8e8c77366ae0d577371fae04c1cf7d29
Armelio Nofulla <nofullaarmelio01@gmail.com>
2024-04-16 20:23:22 +0200

    ading orders page and payment method using paypal sandbox

--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse
+import datetime
+
+from django.shortcuts import render, redirect
+
 from cart.models import CartItem
 from .forms import OrderForm
 from .models import Order
-import datetime
+
+
 # Create your views here.
+
+def payments(request):
+    return render(request, 'orders/payments.html')
+
+
 def place_order(request, total=0, quantity=0):
     current_user = request.user
 
@@ -46,15 +54,21 @@ def place_order(request, total=0, quantity=0):
             yr = int(datetime.date.today().strftime('%Y'))
             dt = int(datetime.date.today().strftime('%d'))
             mt = int(datetime.date.today().strftime('%m'))
-            d = datetime.date(yr,mt,dt)
+            d = datetime.date(yr, mt, dt)
             current_date = d.strftime("%Y%m%d")
             order_number = current_date + str(data.id)
             data.order_number = order_number
             data.save()
-            return redirect('checkout')
+
+            order = Order.objects.get(user=current_user, is_ordered=False, order_number=order_number)
+            context = {
+                'order': order,
+                'cart_items': cart_items,
+                'total': total,
+                'tax': tax,
+                'grand_total': grand_total
+            }
+
+            return render(request, 'orders/payments.html', context)
         else:
             return redirect('checkout')
-
-
-
-
