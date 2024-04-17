64dcbc0f6a8cc97b536bb42abcc9811a0c9906b7
Armelio Nofulla <146014177+Armelionofulla@users.noreply.github.com>
2024-04-17 11:25:36 +0200

    Update settings.py

--- eCommerceTech/settings.py
+++ eCommerceTech/settings.py
@@ -148,8 +148,8 @@ MESSAGE_TAGS = {
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'nofullaarmelio01@gmail.com'
-EMAIL_HOST_PASSWORD = 'womievypqcolddhs'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
